ef7c5371a51012c2dc6bc4a30e7d4d76702aa9e6
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-09-08 16:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function fibonacci(num) {
- if (num==1) {
- 	console.log(0)
- }
-	else if (num==2||num==3) {
-		console.log(1)
-	} 
-	else{
-		let n1=1;
-		let n2=1;
-		let ans = 0;
-		for(let i=4; i<=num; i++){
-			ans=n1+n2;
-			n1=n2;
-			n2=ans;
-		}
-		console.log(ans)
-	}
+  if (num == 0) {
+    return 0;
+  } else if (num == 1 || num == 2) {
+    return 1;
+  } else {
+    let n1 = 1;
+    let n2 = 1;
+    let ans = 0;
+    for (let i = 3; i <= num; i++) {
+      ans = n1 + n2;
+      n1 = n2;
+      n2 = ans;
+    }
+    return ans;
+  }
 }
 
 module.exports = fibonacci;
+
