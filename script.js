d4f0f30a6fd618d0fecfbd1efad34a3f9c9b9225
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-09-08 16:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function fibonacci(num) {
-  if (num == 0) {
+  if (num == 1) {
     return 0;
-  } else if (num == 1 || num == 2) {
+  } else if (num == 2 || num == 3) {
     return 1;
   } else {
     let n1 = 1;
     let n2 = 1;
     let ans = 0;
-    for (let i = 3; i <= num; i++) {
+    for (let i = 4; i <= num; i++) {
       ans = n1 + n2;
       n1 = n2;
       n2 = ans;
